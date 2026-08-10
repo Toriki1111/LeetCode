c9abb918f7d95f81251ce58e910feb0410a7623a
Toki <144874838+Toriki1111@users.noreply.github.com>
2026-08-10 16:36:50 +0700

    Time: 0 ms (100.00%) | Memory: 14.1 MB (85.18%) - LeetSync

--- Med/1-two-sum/two-sum.cpp
+++ Med/1-two-sum/two-sum.cpp
@@ -2,7 +2,7 @@ class Solution
 {
     public:
         vector<int> twoSum(vector<int>& nums, int target)
-    {//ew
+    {
         for(int a = 1;  a < nums.size();a++)
         {
             for(int b=a;b<nums.size();b++){
