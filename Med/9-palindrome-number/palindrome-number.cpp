6cc364133b5a820341f198544af907ccdcf51e31
Toki <144874838+Toriki1111@users.noreply.github.com>
2026-08-10 16:36:03 +0700

    Time: 0 ms (100.00%) | Memory: 8.7 MB (37.02%) - LeetSync

---
+++ Med/9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public: //e
+    bool isPalindrome(int x) {
+        long long y,val,z = x;
+        if(x < 0)
+        {
+        return false;
+        }
+        while(z > 0)
+        {
+        y = z % 10;
+        z = z / 10;
+        val = val * 10 + y;
+        }
+        if(val == x)
+        {
+            return true;
+        }
+        return false;
+    }
+};
