c7b3d6f513f7836ef10145f9770307a9cadb356a
Toki <144874838+Toriki1111@users.noreply.github.com>
2026-08-11 10:13:08 +0700

    Time: 14 ms (12.78%) | Memory: 14 MB (96.12%) - LeetSync

--- Med/1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ Med/1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,16 +5,15 @@ public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         long long n = nums.size();
         vector<int> result(n,0);
-        long long count = 0;
         for(int i = 0; i < n ;i++)
-        {
+        {int count = 0;
             for(int j = 0; j < n; j++) {
                 if(nums[j] < nums[i]) {
                     count++;
                 }
         }
         result[i] = count;
-        count = 0;
+        
     }
     return result;
     }
